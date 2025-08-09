6db268c83e3b973d5446ba1eec3641a0edac94b9
kelsonalmeida <kelson.victr@gmail.com>
2025-08-09 13:57:57 -0300

    att

--- src/screens/PedidoForm.tsx
+++ src/screens/PedidoForm.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, StyleSheet, View } from 'react-native'
+import React, { useContext, useState } from 'react'
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { RootStackParamList } from '../navigation/types'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { Button, Text, TextInput } from 'react-native-paper'
+import { AuthContext } from '../contexts/AuthContext'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase/config'
 
 type FormRouteProp = RouteProp<RootStackParamList, 'NovoPedido'>
 type FormNavigationProp = NativeStackNavigationProp<RootStackParamList, 'NovoPedido'>
@@ -16,6 +19,34 @@ const PedidoForm = () => {
 
     const route = useRoute<FormRouteProp>()
     const navigation = useNavigation<FormNavigationProp>()
+
+    const { user } = useContext(AuthContext)
+
+    const handleEnviar = async () => {
+        if (!user) return
+        if (!mesa || !itens) {
+            Alert.alert('Atenção', 'Mesa e itens são obrigatórios')
+            return
+        }
+
+        try {
+            await addDoc(collection(db, 'pedidos'), {
+                mesa,
+                itens,
+                observacoes,
+                criadoEm: serverTimestamp(),
+                uid: user.uid,
+                status: 'solicitado'
+            })
+            Alert.alert('Sucesso', 'Pedido solicitado com sucesso!')
+            navigation.goBack()
+        } catch (error: any) {
+          console.log("Erro ao cadastrar:", error)
+          console.log("Código do erro:", error.code)
+          console.log("Mensagem do erro:", error.message)
+          Alert.alert('Erro', 'Não foi possível salvar o pedido')
+        }
+    }
 
   return (
     <View style={{ padding: 20 }}>
@@ -41,7 +72,7 @@ const PedidoForm = () => {
         style={{ marginBottom: 10 }}
       />
 
-      <Button mode="contained">
+      <Button mode="contained" onPress={handleEnviar}>
         Solicitar Pedido
       </Button>
     </View>
