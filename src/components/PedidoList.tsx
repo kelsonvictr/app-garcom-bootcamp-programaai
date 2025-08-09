7ee87ae330b1fe903984209fe4478c6d1ef4cee4
kelsonalmeida <kelson.victr@gmail.com>
2025-08-09 14:31:22 -0300

    att

---
+++ src/components/PedidoList.tsx
@@ -0,0 +1,86 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Pedido } from '../types/Pedido'
+import { AuthContext } from '../contexts/AuthContext'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigation/types'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import { db } from '../firebase/config'
+import { Card, Chip, Button as PaperButton } from 'react-native-paper'
+
+const PedidoList = () => {
+
+    const [pedidos, setPedidos] = useState<Pedido[]>([])
+    const { user } = useContext(AuthContext)
+
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
+    useEffect(() => {
+        if (!user) return
+        const q = query(collection(db, 'pedidos'), where('uid', '==', 'user.uid'))
+        const unsub = onSnapshot(q, snap => {
+            const lista: Pedido[] = []
+            snap.forEach(d => {
+                const data = d.data() as any
+                lista.push({
+                    id: d.id,
+                    mesa: data.mesa,
+                    itens: data.itens,
+                    observacoes: data.observacoes || "",
+                    criadoEm: data.criadoEm,
+                    uid: data.uid,
+                    status: data.status || "solicitado"
+                })
+            })
+            setPedidos(lista)
+        })
+        return () => unsub()    
+
+    }, [user])
+
+  const renderItem = ({ item }: { item: Pedido }) => (
+    <Card
+      style={{
+        marginVertical: 6,
+        marginHorizontal: 10,
+        opacity: item.status === 'entregue' ? 0.5 : 1
+      }}
+    >
+      <Card.Title
+        title={`Mesa ${item.mesa}`}
+        right={() => (
+          <Chip style={{ marginRight: 8 }}>
+            {item.status.toUpperCase()}
+          </Chip>
+        )}
+      />
+      <Card.Content>
+        <Text>Itens: {item.itens}</Text>
+        {item.observacoes ? <Text>Obs: {item.observacoes}</Text> : null}
+      </Card.Content>
+      <Card.Actions>
+        <PaperButton>
+          Excluir
+        </PaperButton>
+        {item.status === 'preparado' && (
+          <PaperButton>
+            Marcar Entregue
+          </PaperButton>
+        )}
+      </Card.Actions>
+    </Card>
+  )
+
+  return (
+    <FlatList
+      data={pedidos}
+      keyExtractor={i => i.id!}
+      renderItem={renderItem}
+    />
+  )
+}
+
+export default PedidoList
+
+const styles = StyleSheet.create({})
